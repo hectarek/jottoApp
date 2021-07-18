a47791ca76bf569cb1d5f1715b0e69bde37043d2
Hector Gonzalez <hectarek13@gmail.com>
2021-07-18 15:53:59 -0400

    added guess words

--- jotto/src/components/GuessedWords.test.js
+++ jotto/src/components/GuessedWords.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { checkProps, findByTestAttr } from '../test/testUtils';
 import GuessedWords from './GuessedWords';
 import { test } from '@jest/globals';
 
@@ -10,9 +10,29 @@ const defaultProps = {
 
 const setup = (props={}) => {
     const setupProps = {...defaultProps, ...props};
-    return shallow(<GuessedWords />)
+    return shallow(<GuessedWords {...setupProps} />)
 }
 
 test('does not throw warning', () => {
+    checkProps(GuessedWords, defaultProps);
+})
 
+describe("if there are no words guessed", () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({guessedWords: []});
+    })
+
+    test("renders without error", () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+    test("renders instructions to guess words", () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions');
+        expect(instructions.text()).not.toBe(0);
+    });
+})
+
+describe("if there are words guessed", () => {
+    
 })
