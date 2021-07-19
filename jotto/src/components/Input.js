c0274c0093b2468c429cac30883217b4fb929b8c
Hector Gonzalez <hectarek13@gmail.com>
2021-07-19 16:55:04 -0400

    more input tests

--- jotto/src/components/Input.js
+++ jotto/src/components/Input.js
@@ -1,20 +1,37 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const Input = (props) => {
+const Input = ({ secretWord }) => {
+
+    const [currentGuess, setCurrentGuess] = React.useState('')
+
+    const handleChange = (e) => {
+        setCurrentGuess(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setCurrentGuess('');
+    }
+
 	return (
 		<form className="row g-3" data-test="guess-input">
             <div className="col-auto">
                 <h4>Enter a Guess:</h4>
             </div>
             <div className="col-auto">
-                <label for="guessWorkInput" className="visually-hidden">Guess a word</label>
-                <input type="password" className="form-control" id="guessWorkInput" placeholder="Your word" />
+                <label htmlFor="guessWorkInput" className="visually-hidden">Guess a word</label>
+                <input data-test="input-box" type="text" className="form-control" id="guessWorkInput" value={currentGuess} onChange={(e) => handleChange(e)} placeholder="Your word" />
             </div>
             <div className="col-auto">
-                <button type="submit" className="btn btn-primary mb-3">Guess</button>
+                <button data-test="submit-button" type="submit" className="btn btn-primary mb-3" onClick={(e) => handleSubmit(e)}>Guess</button>
             </div>
         </form>
 	);
 };
+
+Input.protoTypes = {
+    secretWord: PropTypes.string.isRequired,
+}
 
 export default Input;
--- jotto/src/components/Input.test.js
+++ jotto/src/components/Input.test.js
@@ -3,12 +3,63 @@ import { shallow } from 'enzyme';
 import { checkProps, findByTestAttr } from '../test/testUtils';
 import Input from './Input';
 
-const setup = (props={}) => {
-    const setupProps = {...props};
-    return shallow(<Input {...setupProps} />)
+// // mock entire module for destructuring useState on import.
+// const mockSetCurrentGuess = jest.fn();
+// jest.mock('react', () => ({
+//     ...jest.requireActual('react'),
+//     useState: (initialState) => [initialState, mockSetCurrentGuess]
+// }))
+
+const defaultProps = { secretWord: "party" };
+
+/**
+ * Setup function for app component
+ * @returns {ShallowWrapper}
+ */
+const setup = (secretWord='party') => {
+    return shallow(<Input secretWord={secretWord} />)
 }
 
+test('does not throw warning', () => {
+    checkProps(Input, defaultProps)
+})
+
 test('renders without error', () => {
-    const wrapper = setup();
+    const wrapper = setup({success: true});
     const input = findByTestAttr(wrapper, 'guess-input')
+    expect(input.length).toBe(1);
+})
+
+describe('state controlled input field', () => {
+
+    let mockSetCurrentGuess = jest.fn();
+    let wrapper; 
+    let originalUseState;
+
+    beforeEach(() => {
+        mockSetCurrentGuess.mockClear();
+        originalUseState = React.useState;
+        React.useState = jest.fn(() => ["", mockSetCurrentGuess])
+        wrapper = setup();
+    })
+
+    afterEach(() => {
+        React.useState = originalUseState;
+    });
+
+    test('state updates with value of input box upon change', () => {
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+        
+        // Do a mock event to say this 
+        const mockEvent = { target: {value: 'train'} };
+        inputBox.simulate('change', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    })
+    test('state is cleared when submit is clicked', () => {
+        const submitButton = findByTestAttr(wrapper, 'submit-button');
+        
+        submitButton.simulate('click', { preventDefault() {} });
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('')
+    })
 })
